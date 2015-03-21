2288fabb7bbd5bb053938d0e7f50d7d124e66f79
edgarallang <edgarallan182@gmail.com>
2015-03-20 21:22:03 -0400

    modifying tree

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Allan/Develop/dop-backend/fbone/env"
+set -gx VIRTUAL_ENV "/var/www/fbone/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
