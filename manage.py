4612cc39f803cd15e8cac5f304f1b0eb63273562
Edgar Allan Glez <Allan@Edgars-MacBook-Pro.local>
2015-03-27 17:36:18 -0700

    push to pikochas

--- manage.py
+++ manage.py
@@ -26,13 +26,13 @@ def initdb():
     # db.drop_all()
     db.create_all()
 
-    admin = BranchUser(
-            branch_id=2,
-            name=u'Edgar',
-            email=u'admin@fucking.com',
-            password=u'123456')
-    db.session.add(admin)
-    db.session.commit()
+    #admin = BranchUser(
+    #        branch_id=2,
+    #        name=u'Edgar',
+    #        email=u'admin@fucking.com',
+    #        password=u'123456')
+    #db.session.add(admin)
+    #db.session.commit()
 
 
 manager.add_option('-c', '--config',
