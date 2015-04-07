f989e78cced460523da6061040f374fcf55946b2
Edgar Allan Glez <edgarallan182@gmail.com>
2015-04-06 19:53:35 -0600

    tabs bug

--- fbone/badge/models.py
+++ fbone/badge/models.py
@@ -4,10 +4,10 @@ from ..extensions import db, jwt
 from ..utils import get_current_time, SEX_TYPE, STRING_LEN
 from ..level import Level
 
-class Badge(db.Model, UserMixin):
-    __tablename__ = 'badges'
-    badge_id = Column(db.Integer, primary_key=True)
-    name = Column(db.String(STRING_LEN), nullable=False, unique=True)
-    info = Column(db.String(STRING_LEN), nullable=False)
+class Badge(db.Model):
+    __tablename__ = 'badges'
+    badge_id = Column(db.Integer, primary_key=True)
+    name = Column(db.String(STRING_LEN), nullable=False, unique=True)
+    info = Column(db.String(STRING_LEN), nullable=False)
     
     level = db.relationship("Level", uselist=False, backref="badges")
