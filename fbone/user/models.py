0a50f4622a137255a8b7079b34a008dbd851c444
Edgar Allan Glez <edgarallan182@gmail.com>
2015-04-07 13:01:42 -0600

    add imports

--- fbone/user/models.py
+++ fbone/user/models.py
@@ -4,7 +4,6 @@ from sqlalchemy import Column, types
 from sqlalchemy.ext.mutable import Mutable
 from werkzeug import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
-from ../level import Level
 from ..extensions import db
 from ..utils import get_current_time, SEX_TYPE, STRING_LEN
 from .constants import USER, USER_ROLE, ADMIN, INACTIVE, USER_STATUS
