293afffa446b0adbd79aeb20b2c89546c0c7a219
Edgar Allan Glez <edgarallan182@gmail.com>
2015-04-21 16:35:56 -0600

    new calis api

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -72,9 +72,9 @@ def get_coupon(coupon_id):
 
 @coupon.route('/get/all', methods = ['GET'])
 def get_all_coupon():
-    list_coupon = Coupon.query.join(BondCoupon.coupon).\
-                               join(DiscountCoupon.coupon).\
-                               join(NxNCoupon.coupon).\
+    list_coupon = Coupon.query.join(BondCoupon).\
+                               join(DiscountCoupon).\
+                               join(NxNCoupon).\
                                filter(Coupon.coupon_id)
 
     selected_coupon = coupon_schema.dump(list_coupon)
