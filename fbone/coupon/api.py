b1d1c6f5df8e37738d6b807db628996b72619603
jequintero <eduardo.quintero52@gmail.com>
2015-05-29 17:21:16 -0600

    used coupons

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -232,7 +232,7 @@ def get_used_coupons_by_user():
         user_id = User.query.get(payload['id']).user_id
 
         users = db.engine.execute("SELECT coupons.branch_id,coupons.coupon_id,branches_design.logo,coupons.name,clients_coupon.latitude,clients_coupon.longitude \
-                                    , users.names, users.surnames, users.user_id, users_image.main_image FROM clients_coupon \
+                                    , users.names, users.surnames, users.user_id, users_image.main_image, branches.name AS branch_name FROM clients_coupon \
                                     INNER JOIN users ON clients_coupon.user_id=users.user_id  \
                                     INNER JOIN users_image ON users.user_id = users_image.user_id \
                                     INNER JOIN coupons ON clients_coupon.coupon_id = coupons.coupon_id \
@@ -244,6 +244,6 @@ def get_used_coupons_by_user():
 
         return jsonify({'data': users_list.data})
 
-    return jsonify({'message': 'Oops! algo salió mal, seguramente fue tu tarjeta sobregirada'})
+    return jsonify({'message': 'Oops! algo salió mal'})
 
 
