eeb9b52f0b8d3b4ab8980d7e633a4ee1a49fb481
edgarallang <edgarallan182@gmail.com>
2015-06-02 14:33:54 -0600

    api de establecimientos mas cercanos

--- fbone/company/models.py
+++ fbone/company/models.py
@@ -56,8 +56,8 @@ class BranchLocation(db.Model):
     city = Column(db.String(STRING_LEN), nullable=False)
     address = Column(db.String(STRING_LEN), nullable=False)
 
-    branch = db.relationship('Branch',
-                        backref=db.backref("branches_location", lazy="dynamic"))
+    # branch = db.relationship('Branch',
+    #                     backref=db.backref("branches_location", lazy="dynamic"))
 
 # =====================================================================
 # Categories 
