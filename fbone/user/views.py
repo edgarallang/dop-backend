9d3816539cbbe145d783749c55b65f3064215571
edgarallang <edgarallan182@gmail.com>
2015-06-03 14:20:20 -0600

    api rota pikochas

--- fbone/user/views.py
+++ fbone/user/views.py
@@ -46,7 +46,7 @@ def profile(userId):
     # query = 'SELECT * FROM users INNER JOIN users_image \
     #          ON users.user_id = users_image.user_id WHERE users.user_id=' + userId
     
-    selectedUser = session.query(User).\
+    selectedUser = db.session.query(User).\
                    join(User.users_image_user_id).\
                    filter_by(User.id== userId).first()
 
