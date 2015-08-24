c280c70f257b18f59bf344905addcf6e114e229b
edgarallang <edgarallan182@gmail.com>
2015-08-24 12:15:04 -0600

    tomar ocupon api

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -66,7 +66,7 @@ def take_coupon():
         db.session.add(user_take)
         db.session.commit()
         import pdb; pdb.set_trace()
-        folio = request.json['branch_id'] + user_take.clients_coupon_id + request.json['taken_date']
+        folio = '%d%s%d' % (request.json['branch_id'], request.json['folio_date'], user_take.clients_coupon_id)
         user_take.folio = folio
         db.session.commit()
 
