63ee6560419c0d3370213809c8ca6171ed5a97f8
jequintero <eduardo.quintero52@gmail.com>
2015-08-25 18:20:41 -0600

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -248,6 +248,7 @@ def get_used_coupons_by_coupon():
     if request.headers.get('Authorization'):
         token_index = True
         payload = parse_token(request, token_index)
+        coupon_id = request.json['coupon_id']
         query = 'SELECT coupons.branch_id,coupons.coupon_id,branches_design.logo,coupons.name,clients_coupon.clients_coupon_id,clients_coupon.latitude,clients_coupon.longitude \
                                     , users.names, users.surnames, users.user_id, users_image.main_image, branches.name AS branch_name, \
                                     (SELECT COUNT(*)  FROM clients_coupon_likes WHERE clients_coupon.clients_coupon_id = clients_coupon_likes.clients_coupon_id) AS total_likes, \
@@ -258,7 +259,7 @@ def get_used_coupons_by_coupon():
                                     INNER JOIN coupons ON clients_coupon.coupon_id = coupons.coupon_id AND coupons.coupon_id = %d \
                                     INNER JOIN branches ON coupons.branch_id = branches.branch_id  \
                                     INNER JOIN branches_design ON coupons.branch_id = branches_design.branch_id \
-                                    ORDER BY taken_date DESC' % (payload['id'],request.json['coupon_id'])
+                                    ORDER BY taken_date DESC' % (payload['id'], coupon_id)
         users = db.engine.execute(query)
         users_list = user_join_exchanges_coupon_schema.dump(users)
 
