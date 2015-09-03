dbaeaf1c4a8c7c440378f8d823b9b1d24db950d4
jequintero <eduardo.quintero52@gmail.com>
2015-09-02 19:01:44 -0600

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -248,6 +248,7 @@ def get_used_coupons_by_user_likes():
         token_index = True
         payload = parse_token(request, token_index)
         user_id = User.query.get(payload['id']).user_id
+        limit = request.args.get('limit')
 
         users = db.engine.execute('SELECT coupons.branch_id,coupons.coupon_id,branches_design.logo,coupons.name,clients_coupon.clients_coupon_id,clients_coupon.latitude,clients_coupon.longitude \
                                     , users.names, users.surnames, users.user_id, users_image.main_image, branches.name AS branch_name, \
@@ -259,7 +260,34 @@ def get_used_coupons_by_user_likes():
                                     INNER JOIN coupons ON clients_coupon.coupon_id = coupons.coupon_id \
                                     INNER JOIN branches ON coupons.branch_id = branches.branch_id \
                                     INNER JOIN branches_design ON coupons.branch_id = branches_design.branch_id \
-                                    ORDER BY taken_date DESC' % payload['id'])
+                                    ORDER BY start_date DESC LIMIT %s OFFSET 0 ORDER BY clients_coupon.clients_coupon_id DESC' % payload['id'],limit)
+
+        users_list = user_join_exchanges_coupon_schema.dump(users)
+
+        return jsonify({'data': users_list.data})
+
+    return jsonify({'message': 'Oops! algo salió mal'})
+
+@coupon.route('/used/get/user/offset', methods=['GET'])
+def get_used_coupons_by_user_likes_offset():
+    if request.headers.get('Authorization'):
+        token_index = True
+        payload = parse_token(request, token_index)
+        user_id = User.query.get(payload['id']).user_id
+        offset = request.args.get('offset')
+        client_coupon_id = request.args.get('coupon_id')
+
+        users = db.engine.execute('SELECT coupons.branch_id,coupons.coupon_id,branches_design.logo,coupons.name,clients_coupon.clients_coupon_id,clients_coupon.latitude,clients_coupon.longitude \
+                                    , users.names, users.surnames, users.user_id, users_image.main_image, branches.name AS branch_name, \
+                                    (SELECT COUNT(*)  FROM clients_coupon_likes WHERE clients_coupon.clients_coupon_id = clients_coupon_likes.clients_coupon_id) AS total_likes, \
+                                    (SELECT COUNT(*)  FROM clients_coupon_likes WHERE clients_coupon_likes.user_id = %d AND clients_coupon_likes.clients_coupon_id = clients_coupon.clients_coupon_id) AS user_like \
+                                    FROM clients_coupon \
+                                    INNER JOIN users ON clients_coupon.user_id=users.user_id  \
+                                    INNER JOIN users_image ON users.user_id = users_image.user_id \
+                                    INNER JOIN coupons ON clients_coupon.coupon_id = coupons.coupon_id \
+                                    INNER JOIN branches ON coupons.branch_id = branches.branch_id \
+                                    INNER JOIN branches_design ON coupons.branch_id = branches_design.branch_id \
+                                    WHERE clients_coupon.clients_coupon_id < %s ORDER BY start_date DESC LIMIT 6 OFFSET %s ORDER BY clients_coupon.clients_coupon_id DESC' % payload['id'],client_coupon_id,offset)
 
         users_list = user_join_exchanges_coupon_schema.dump(users)
 
