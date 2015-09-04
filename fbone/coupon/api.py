c4ddb4ae5930319e0eb1c4a05e98af63302b2776
jequintero <eduardo.quintero52@gmail.com>
2015-09-04 16:57:49 -0600

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -475,8 +475,8 @@ def search_all_coupon_user_offset():
     token_index = True
     offset = request.json['offset']
     coupon_id = request.json['coupon_id']
-    text = request.json['text']
-
+    #text = request.json['text']
+    text = "Cu"
     payload = parse_token(request, token_index)
 
     list_coupon = db.engine.execute('SELECT *, \
@@ -488,7 +488,7 @@ def search_all_coupon_user_offset():
                                     coupons.branch_id = branches_design.branch_id \
                                     INNER JOIN branches ON coupons.branch_id = branches.branch_id \
                                     INNER JOIN branches_location on coupons.branch_id = branches_location.branch_id \
-                                    WHERE deleted = false AND coupons.name ILIKE %s ORDER BY start_date DESC LIMIT 6' % (payload['id'],"Cu%"))
+                                    WHERE deleted = false AND coupons.name ILIKE %s ORDER BY start_date DESC LIMIT 6' % (payload['id'],text))
 
 
 
