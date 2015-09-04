63ee62862706c2d18af9f9b99d44a1b68e1d5000
jequintero <eduardo.quintero52@gmail.com>
2015-09-04 15:48:27 -0600

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -468,4 +468,27 @@ def create_nxn(request):
         return customizationSuccess
     return customizationSuccess
 
+#SEARCH API
+@coupon.route('/search/get/user/offset/', methods = ['GET'])
+def search_all_coupon_user_offset():
+    #user_id = request.args.get('user_id')
+    token_index = True
+    offset = request.args.get('offset')
+    coupon_id = request.args.get('coupon_id')
+    payload = parse_token(request, token_index)
 
+    list_coupon = db.engine.execute('SELECT *, \
+                                    (SELECT COUNT(*)  FROM coupons_likes \
+                                        WHERE coupons.coupon_id = coupons_likes.coupon_id) AS total_likes, \
+                                    (SELECT COUNT(*)  FROM coupons_likes \
+                                        WHERE coupons_likes.user_id = %d AND coupons.coupon_id = coupons_likes.coupon_id) AS user_like \
+                                    FROM coupons INNER JOIN branches_design ON \
+                                    coupons.branch_id = branches_design.branch_id \
+                                    INNER JOIN branches ON coupons.branch_id = branches.branch_id \
+                                    INNER JOIN branches_location on coupons.branch_id = branches_location.branch_id \
+                                    WHERE deleted = false AND coupons.coupon_id < %s ORDER BY start_date DESC LIMIT 6 OFFSET %s' % (payload['id'],coupon_id,offset))
+
+
+
+    selected_list_coupon = coupons_logo_schema.dump(list_coupon)
+    return jsonify({'data': selected_list_coupon.data})
