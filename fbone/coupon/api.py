8bc1e7b6bf9a5fa89aac1bbf655adc2b45a49c0c
jequintero <eduardo.quintero52@gmail.com>
2015-09-04 23:05:50 -0600

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -488,7 +488,7 @@ def search_all_coupon_user_offset():
             coupons.branch_id = branches_design.branch_id \
             INNER JOIN branches ON coupons.branch_id = branches.branch_id \
             INNER JOIN branches_location on coupons.branch_id = branches_location.branch_id \
-            WHERE deleted = false AND coupons.name ILIKE '%s' ORDER BY start_date DESC" , (payload['id'],"%" + "sta" + "%",))
+            WHERE deleted = false AND coupons.name ILIKE %s ORDER BY start_date DESC" , (payload['id'],"%" + "sta" + "%",))
 
     #list_coupon = db.engine.execute(query)
     #list_coupon = db.engine.execute("SELECT * FROM branches WHERE name ILIKE %s LIMIT 1", ("%" + "sta" + "%",))
