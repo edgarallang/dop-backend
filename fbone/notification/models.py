28057c7be6ce6f62e59a5aa383b6b77b655de848
jequintero <eduardo.quintero52@gmail.com>
2015-10-13 13:00:26 -0600

    clean

--- fbone/notification/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from marshmallow import Schema, fields, ValidationError
-from sqlalchemy import Column, types
-from sqlalchemy.ext.mutable import Mutable
-from werkzeug import generate_password_hash, check_password_hash
-from flask.ext.login import UserMixin
-from ..extensions import db
-from ..utils import get_current_time, SEX_TYPE, STRING_LEN
-from .constants import USER, USER_ROLE, ADMIN, INACTIVE, USER_STATUS
