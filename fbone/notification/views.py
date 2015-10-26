baa8c79d8bcdb3f0592cb59740e87ae4281de9a4
jequintero <eduardo.quintero52@gmail.com>
2015-10-26 12:26:57 -0700

    clean

--- fbone/notification/views.py
+++ fbone/notification/views.py
@@ -84,10 +84,10 @@ def test_message(message):
 
 @socketio.on('connect', namespace="/test")
 def test_connect(user_token):
-    session["id"] = 5
+    room = parse_token_socket(session["token"])
     #join_room(session["id"])
     #send(' has entered the room.', room=session["id"])
-    print "conectado "
+    print "conectado "+room
     #emit('my response', {'data': 'Connected'})
 
 @socketio.on('disconnect', namespace='/test')
