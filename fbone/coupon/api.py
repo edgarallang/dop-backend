3d5caa18709652d752ba0a08be50e5516711454f
jequintero <eduardo.quintero52@gmail.com>
2015-10-26 15:47:15 -0700

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -533,7 +533,7 @@ def like_used_coupon():
 
             liked_user = ClientsCoupon.query.filter_by(clients_coupon_id = request.json['clients_coupon_id']).first()
 
-            socketio.emit('notification',{'data': 'someone triggered me'},namespace='/app')
+            socketio.emit('notification',{'data': 'someone triggered me'},namespace='/app',room=liked_user.user_id)
 
 
             db.session.add(user_like)
