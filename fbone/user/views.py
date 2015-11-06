36be8d6f9e03e61712c4efa56aa54931be82db36
jequintero <eduardo.quintero52@gmail.com>
2015-11-06 11:21:17 -0700

    clean

--- fbone/user/views.py
+++ fbone/user/views.py
@@ -378,6 +378,6 @@ def get_used_coupons_by_user_likes_offset():
 
     return jsonify({'message': 'Oops! algo salió mal'})
 
-@user.route('/<int:user_id>/<int:exp>/set', methods=['GET'])
+@user.route('/<int:user_id>/<string:exp>/set', methods=['GET'])
 def set_experience():
-    return jsonify({'message': 'experiencia asignada'})
+    return jsonify({'message': 'experiencia asignada %s' % exp})
