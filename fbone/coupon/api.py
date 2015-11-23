1010f0c3435792e707cfae6e1b248247f84ee279
jequintero <eduardo.quintero52@gmail.com>
2015-11-23 11:13:48 -0700

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -100,7 +100,7 @@ def use_coupon():
 
     if client_coupon_json.data['branch_id'] == qr_code:
         print "Good"
-    else
+    else:
         print "Bad"
     #client_coupon.used = True
     #client_coupon.used_date = datetime.now()
