55662410d72e0f109fd81160c00a1942b14f1659
jequintero <eduardo.quintero52@gmail.com>
2015-11-24 13:26:13 -0700

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -89,7 +89,7 @@ def use_coupon():
     client_coupon_json = clients_coupon_inner_coupon_schema.dump(client_coupon)
     
     if client_coupon_json.data['branch_id'] == qr_code:
-        client_coupon = ClientsCoupon.query.filter_by(clients_coupon_id = client_coupon.clients.coupon_id).first()
+        client_coupon = ClientsCoupon.query.filter_by(clients_coupon_id = client_coupon.clients_coupon_id).first()
         client_coupon.used = True
         client_coupon.used_date = datetime.now()
         db.session.commit()
