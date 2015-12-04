d8a92b87a2f7b2f452220105b19301b5c751f75f
edgarallang <edgarallan182@gmail.com>
2015-12-04 11:58:28 -0700

    people schema

--- fbone/user/models.py
+++ fbone/user/models.py
@@ -179,18 +179,19 @@ class FriendsCountSchema(Schema):
                   'total')
 
 class PeopleSchema(Schema):
-    user_id = fields.Number()
-    names = fields.String()
-    surnames = fields.String()
-    birth_date = fields.String()
-    facebook_key = fields.String()
-    google_key = fields.String()
-    twitter_key = fields.String()
-    privacy_status = fields.Number()
-    exp = fields.Number()
-    user_image_id = fields.Number()
-    main_image = fields.String()
-    friend = fields.Boolean()
+    class Meta:
+        fields = ('user_id',
+                  'names',
+                  'surnames',
+                  'birth_date',
+                  'facebook_key',
+                  'google_key',
+                  'twitter_key',
+                  'privacy_status',
+                  'exp',
+                  'user_image_id',
+                  'main_image',
+                  'friend')
 
 class UserJoinExchanges(Schema):
     class Meta:
