bcc89f512bfe11f35b5c8808f379e4668595b43f
Edgar Allan Glez <edgarallan182@gmail.com>
2016-02-08 12:22:13 -0700

    friends api

--- fbone/user/views.py
+++ fbone/user/views.py
@@ -198,7 +198,7 @@ def add_friend():
         user_two = User.query.get(user_to_add)
         friendshipExist = Friends.query.filter(((Friends.user_one_id == user_id) & (Friends.user_two_id == user_to_add))).all()
         if not friendshipExist:
-            user_two = User.query.get(user_to_add)
+            user_two = User.query.get(user_to_add).first()
             print user_two
             if user_two.privacy_status == 0:
                 operation_id = 1
