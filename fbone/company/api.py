bc8565f150750b46ece96c8fca7c21b95d69383d
jequintero <eduardo.quintero52@gmail.com>
2016-02-11 18:21:58 -0700

    clean

--- fbone/company/api.py
+++ fbone/company/api.py
@@ -127,7 +127,7 @@ def nearest_branches():
 
     query = 'SELECT DISTINCT ON (branch_id) branch_location_id, branch_id, state, city, latitude, longitude, distance, address, name, category_id, logo, company_id \
                 FROM (SELECT z.branch_location_id, z.branch_id, z.state, z.city, z.address,branches_design.logo, \
-                    z.latitude, z.longitude, branches.name, subcategory.category_id, \
+                    z.latitude, z.longitude, branches.name, subcategory.category_id, branches.company_id, \
                     p.radius, \
                     p.distance_unit \
                              * DEGREES(ACOS(COS(RADIANS(p.latpoint)) \
