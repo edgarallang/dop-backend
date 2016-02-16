55522cc123937bcfa4c97e6904d0d0ba83c4f015
jequintero <eduardo.quintero52@gmail.com>
2016-02-16 10:57:33 -0700

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -657,7 +657,7 @@ def get_used_coupons_by_user_likes_offset():
                                     INNER JOIN coupons ON clients_coupon.coupon_id = coupons.coupon_id \
                                     INNER JOIN branches ON coupons.branch_id = branches.branch_id \
                                     INNER JOIN branches_design ON coupons.branch_id = branches_design.branch_id \
-                                    WHERE clients_coupon.used = true AND clients_coupon.clients_coupon_id < %s ORDER BY user_date DESC LIMIT 6 OFFSET %s' % (payload['id'], client_coupon_id , offset))
+                                    WHERE clients_coupon.used = true AND clients_coupon.clients_coupon_id < %s ORDER BY used_date DESC LIMIT 6 OFFSET %s' % (payload['id'], client_coupon_id , offset))
 
         users_list = user_join_activity_newsfeed.dump(users)
 
