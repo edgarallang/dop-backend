0ce4401ab88bcfc570c10f71b82fe5b8f94a43e0
jequintero <eduardo.quintero52@gmail.com>
2016-03-18 11:26:01 -0700

    clean

--- fbone/company/api.py
+++ fbone/company/api.py
@@ -102,7 +102,7 @@ def select_branch_profile(branch_id):
 @company.route('/me', methods = ['POST'])    
 def select_branch_user():
     branch_data = 'SELECT branches_user.branches_user_id, branches_user.branch_id, \
-                    branches_user.name, branches_user.email, branches.name, \
+                    branches_user.name, branches_user.email, \
                     branches_location.latitude, branches_location.longitude FROM branches_user \
                     INNER JOIN branches ON branches_user.branch_id = branches.branch_id \
                     INNER JOIN branches_location ON branches_user.branch_id = branches_location.branch_id'
