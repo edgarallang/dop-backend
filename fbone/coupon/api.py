914d3535236747d558a9b662c0440f64130a3ddb
jequintero <eduardo.quintero52@gmail.com>
2016-04-04 11:20:37 -0600

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -67,8 +67,8 @@ def level_up(user_id):
 @coupon.route('/generate/pdf', methods=['GET'])
 def generate_pdf():
     pdf_data = "<html><body><p>To PDF or not to PDF<p></body></html>"
-    pdf = StringIO()
-    pisa.CreatePDF(StringIO(pdf_data), pdf)
+    pdf = cStringIO.StringIO()
+    pisa.CreatePDF(pdf(pdf_data), pdf)
     return pdf
 # POST methods
 
