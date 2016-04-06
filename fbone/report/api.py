7af884ecc39d3a6360108a2a874a6a48decdf059
jequintero <eduardo.quintero52@gmail.com>
2016-04-06 12:21:37 -0600

    Report

--- fbone/report/api.py
+++ fbone/report/api.py
@@ -33,15 +33,16 @@ def parse_token(req, token_index):
 
 @report.route('/uses/<int:coupon_id>', methods=['GET'])
 def uses_by_coupon(coupon_id):
-    #if request.headers.get('Authorization'):
-    token_index = False
+    if request.headers.get('Authorization'):
+        token_index = False
+        payload = parse_token(request, token_index)
 
-    report_query = db.engine.execute("SELECT date_trunc('day', clients_coupon.used_date) as day, count(*) \
-                                FROM clients_coupon \
-                                INNER JOIN coupons ON clients_coupon.coupon_id = coupons.coupon_id AND clients_coupon.used = TRUE \
-                                WHERE clients_coupon.coupon_id = %d \
-                                GROUP BY day" % (coupon_id) )
+        report_query = db.engine.execute("SELECT date_trunc('day', clients_coupon.used_date) as day, count(*) \
+                                    FROM clients_coupon \
+                                    INNER JOIN coupons ON clients_coupon.coupon_id = coupons.coupon_id AND clients_coupon.used = TRUE \
+                                    WHERE clients_coupon.coupon_id = %d \
+                                    GROUP BY day" % (coupon_id) )
 
-    report = report_schema.dump(report_query)
-    return jsonify({'data': report.data})
-    #return jsonify({'message': 'Oops! algo salió mal, intentalo de nuevo, echale ganas'})
+        report = report_schema.dump(report_query)
+        return jsonify({'data': report.data})
+    return jsonify({'message': 'Oops! algo salió mal, intentalo de nuevo, echale ganas'})
