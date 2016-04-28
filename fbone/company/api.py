3fa9238712e0028b8340ed3ecb5b29ca47fa9294
jequintero <eduardo.quintero52@gmail.com>
2016-04-28 17:51:14 -0600

    clean

--- fbone/company/api.py
+++ fbone/company/api.py
@@ -142,10 +142,10 @@ def upload_logo(branchId):
     filename = "Hola.jpg"
 
     data = 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAADBQTFRFA6b1q Ci5/f2lt/9yu3 Y8v2cMpb1/DSJbz5i9R2NLwfLrWbw m T8I8////////SvMAbAAAABB0Uk5T////////////////////AOAjXRkAAACYSURBVHjaLI8JDgMgCAQ5BVG3//9t0XYTE2Y5BPq0IGpwtxtTP4G5IFNMnmEKuCopPKUN8VTNpEylNgmCxjZa2c1kafpHSvMkX6sWe7PTkwRX1dY7gdyMRHZdZ98CF6NZT2ecMVaL9tmzTtMYcwbP y3XeTgZkF5s1OSHwRzo1fkILgWC5R0X4BHYu7t/136wO71DbvwVYADUkQegpokSjwAAAABJRU5ErkJggg=='.replace(' ', '+')
-    imgdata = base64.b64decode(image)
+    imgdata = base64.b64decode(data)
 
     with open(filename, 'wb') as f:
-        f.write(imgdata)
+        f.write(image)
         print "Jeje"
     print "Jojo"
     #image.save(os.path.join('uploads/', filename))
