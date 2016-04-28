3333c0e9728baf625d22687954be21447fd7d572
jequintero <eduardo.quintero52@gmail.com>
2016-04-28 17:54:17 -0600

    clean

--- fbone/company/api.py
+++ fbone/company/api.py
@@ -141,6 +141,7 @@ def upload_logo(branchId):
     image = request.json['file']
     filename = "Hola.jpg"
 
+
     data = 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAADBQTFRFA6b1q Ci5/f2lt/9yu3 Y8v2cMpb1/DSJbz5i9R2NLwfLrWbw m T8I8////////SvMAbAAAABB0Uk5T////////////////////AOAjXRkAAACYSURBVHjaLI8JDgMgCAQ5BVG3//9t0XYTE2Y5BPq0IGpwtxtTP4G5IFNMnmEKuCopPKUN8VTNpEylNgmCxjZa2c1kafpHSvMkX6sWe7PTkwRX1dY7gdyMRHZdZ98CF6NZT2ecMVaL9tmzTtMYcwbP y3XeTgZkF5s1OSHwRzo1fkILgWC5R0X4BHYu7t/136wO71DbvwVYADUkQegpokSjwAAAABJRU5ErkJggg=='.replace(' ', '+')
     imgdata = base64.b64decode(data)
 
@@ -148,6 +149,8 @@ def upload_logo(branchId):
         f.write(image)
         print "Jeje"
     print "Jojo"
+
+    print image
     #image.save(os.path.join('uploads/', filename))
     #print "Jejeje"
 
