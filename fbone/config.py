a06d9198ae19969092621de39487782966e292bf
jequintero <eduardo.quintero52@gmail.com>
2016-05-05 12:52:00 -0600

    Test

--- fbone/config.py
+++ fbone/config.py
@@ -15,7 +15,7 @@ class BaseConfig(object):
     # Get app root path, also can use flask.root_path.
     # ../../config.py
     PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    APNS_CERTIFICATE = '../certs/push.pem'
+    APNS_CERTIFICATE =  PROJECT_ROOT+'/certs/push.pem'
     DEBUG = False
     TESTING = False
     use_reloader = False
