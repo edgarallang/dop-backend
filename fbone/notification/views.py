1faa43a86d4caddf0eb3d0dca3b171cf2e5e5fd1
jequintero <eduardo.quintero52@gmail.com>
2016-05-06 13:01:26 -0600

    clean

--- fbone/notification/views.py
+++ fbone/notification/views.py
@@ -55,9 +55,12 @@ def push_test_global():
     users_list = User.query.filter(User.device_token!=None)
     token_list = device_tokens_schema.dump(users_list)
 
+    token_list_data = token_list.data
+
     tokens = []
 
-
+    for key,value in token_list_data.iteritems():
+        print key
 
     #token = '1124931f005c00b7ce00c4f76d6c75589b37680706190098939ccf7fbd244909'
 
@@ -76,7 +79,7 @@ def push_test_global():
     #client.send([token], alert, **options)
     # Get expired tokens.
     #expired_tokens = client.get_expired_tokens()
-    return jsonify({'data': token_list.data.data})
+    return jsonify({'data': token_list.data})
 
 @notification.route('/test/<int:user_id>', methods=['GET'])
 def test_notification(user_id):
