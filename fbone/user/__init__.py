a03b706b01c3ef341693f8062c9b742e66bfdaee
Edgar Allan Glez <edgarallan182@gmail.com>
2016-05-06 12:45:05 -0600

    new font and bug fixed

--- fbone/user/__init__.py
+++ fbone/user/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .models import User, UserImage, UserLevel
+from .models import *
 from .views import user
 from .constants import USER_ROLE, ADMIN, USER, USER_STATUS, NEW, ACTIVE
