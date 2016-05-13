8ada27551422eebe25a5e1e3cdf8b0faa9722d52
jequintero <eduardo.quintero52@gmail.com>
2016-05-13 11:18:45 -0600

    clean

--- fbone/user/views.py
+++ fbone/user/views.py
@@ -214,6 +214,7 @@ def add_friend():
         user_to_add = request.json['user_two_id']
         user_two = User.query.get(user_to_add)
         friendshipExist = Friends.query.filter(((Friends.user_one_id == user_id) & (Friends.user_two_id == user_to_add))).first()
+        launcher_user_data = User.query.filter_by(user_id = payload['id']).first()
 
         if not friendshipExist:
             #user_two = User.query.get(user_to_add)
