364b1527d8fe7129bead7548a7250a671407d29d
jequintero <eduardo.quintero52@gmail.com>
2016-05-26 13:06:05 -0600

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -753,8 +753,9 @@ def get_used_coupons_by_user_likes_offset():
         offset = request.json['offset']
         used_date = request.json['used_date']
 
-        query = 'SELECT coupons.branch_id,coupons.coupon_id,branches_design.logo,coupons.name,clients_coupon.clients_coupon_id,clients_coupon.latitude,clients_coupon.longitude \
-                                    , users.names, users.surnames, users.user_id, users_image.main_image, branches.name AS branch_name, branches.company_id, clients_coupon.used_date, \
+        query = 'SELECT coupons.branch_id,coupons.coupon_id,branches_design.logo,coupons.name,clients_coupon.clients_coupon_id,clients_coupon.latitude,clients_coupon.longitude, \
+                                          users.names, users.surnames, users.user_id, users.exp, users.level, users_image.main_image, branches.name AS branch_name, \
+                                          branches.company_id, clients_coupon.used_date, friends.operation_id, users.privacy_status, \
                                     (SELECT COUNT(*)  FROM clients_coupon_likes WHERE clients_coupon.clients_coupon_id = clients_coupon_likes.clients_coupon_id) AS total_likes, \
                                     (SELECT COUNT(*)  FROM clients_coupon_likes WHERE clients_coupon_likes.user_id = %d AND clients_coupon_likes.clients_coupon_id = clients_coupon.clients_coupon_id) AS user_like, \
                                     (SELECT EXISTS (SELECT * FROM friends \
@@ -768,7 +769,7 @@ def get_used_coupons_by_user_likes_offset():
                                     LEFT JOIN friends ON friends.user_one_id = %d AND friends.user_two_id = users.user_id \
                                     WHERE clients_coupon.used = true AND clients_coupon.used_date <= %s AND clients_coupon.private = false AND friends.operation_id = 1 ORDER BY used_date DESC LIMIT 6 OFFSET %s' % (user_id, user_id, user_id, "'" + used_date + "'" , offset)
 
-        
+
         users = db.engine.execute(query)
 
         users_list = user_join_activity_newsfeed.dump(users)
