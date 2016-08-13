6b872655af2e7c43da2528036618a90ed8130607
jequintero <eduardo.quintero52@gmail.com>
2016-08-12 22:56:36 -0600

    clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -211,7 +211,7 @@ def use_coupon():
                 user_level = level_up(payload['id'])
                 return jsonify({'data': branch_data.data, 'reward': reward, 'level': user_level })
         else:
-            minutes_left = (actual_date - recently_used.used_date)..total_seconds()
+            minutes_left = (actual_date - recently_used.used_date).total_seconds()
             return jsonify({'message': 'error',"minutes": minutes_left})
     return jsonify({'message': 'Oops! algo salió mal, intentalo de nuevo, echale ganas'})
 
