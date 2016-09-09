8b3f4ad9397374588c45be10e38c331f8300f20d
Edgar Allan Glez <edgarallan182@gmail.com>
2016-09-09 10:36:18 -0600

    privacy status api

--- fbone/user/views.py
+++ fbone/user/views.py
@@ -567,7 +567,7 @@ def get_privacy():
     if request.headers.get('Authorization'):
         token_index = True
         payload = parse_token(request, token_index)
-
+        user = User.query.get(payload['id'])
         return jsonify({'privacy_status': user.privacy_status })
     return jsonify({'message': 'Oops! algo salió mal'})
 
