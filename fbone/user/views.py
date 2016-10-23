9a9fe49a03e723a49929ea61ad3a9920ce54bb44
jequintero <eduardo.quintero52@gmail.com>
2016-10-23 10:26:59 -0600

    clean

--- fbone/user/views.py
+++ fbone/user/views.py
@@ -95,6 +95,7 @@ def avatar(user_id, filename):
 @user.route('/login/facebook', methods=['POST'])
 def facebook_login():
     print request
+    return jsonify({"Hola":"Hola"})
     # facebookUser = User.query.filter_by(facebook_key = request.json['facebook_key']).first()
 
     # is_adult = False
