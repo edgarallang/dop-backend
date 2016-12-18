cd330ee64d96e58e36a2cdb7c4bf38bd8a3b86c0
jequintero <eduardo.quintero52@gmail.com>
2016-12-18 10:54:04 -0700

    Clean

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -319,7 +319,7 @@ def get_all_coupon_by_branch(branch_id):
 
     #list_coupon = Coupon.query.filter_by(branch_id = branch_id).all()
     list_coupon = db.engine.execute('SELECT coupons.*, branches.company_id, \
-                                    ((coupons.available = 0) OR (coupons.end_date < now()) )::bool AS completed, \
+                                    ((coupons.available = 0) OR (coupons.end_date > now()) )::bool AS completed, \
                                     (SELECT end_date::DATE - now()::DATE FROM coupons AS c WHERE coupons.coupon_id = c.coupon_id) AS remaining \
                                     FROM coupons \
                                         INNER JOIN branches ON branches.branch_id = coupons.branch_id \
