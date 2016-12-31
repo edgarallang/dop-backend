eab87ade217e1c60c6dbe551a3d7d84f3bd5cebf
jequintero <eduardo.quintero52@gmail.com>
2016-12-31 09:58:04 -0700

    Js in reset password

--- fbone/static/js/main.js
+++ fbone/static/js/main.js
@@ -5,8 +5,5 @@ function hide_flask_message_container() {
 }
 
 $(document).ready(function() {
-    /* Show and hide flash message. */
-    //$('#flash_message_container').slideDown(function() {
-        //setTimeout(hide_flask_message_container, 3000);
-    //});
+    alert("ALO");
 })
--- fbone/templates/js/main.js
+++ fbone/templates/js/main.js
@@ -9,4 +9,6 @@ $(document).ready(function() {
       autoHeight: true,
       autoplay: true
     });
+
+    alert("JO");
 })
