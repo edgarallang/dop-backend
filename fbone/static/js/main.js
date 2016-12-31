1e88dde8dd1cd2d3eb95ccad4f4d92b553600cd5
jequintero <eduardo.quintero52@gmail.com>
2016-12-31 12:43:51 -0700

    Js in reset password

--- fbone/static/js/main.js
+++ fbone/static/js/main.js
@@ -5,5 +5,26 @@ function hide_flask_message_container() {
 }
 
 $(document).ready(function() {
-    alert("ALO");
+    
+
+	$('#reset_password_form').on('submit', function(e){
+		alert("HEY");
+		e.preventDefault();
+	});
+
+
+
+    function isEmail(email) {
+  		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  		return regex.test(email);
+	}
+
+	function countChar(val) {
+	    var len = val.value.length;
+	    if (len >= 500) {
+	      val.value = val.value.substring(0, 500);
+	    } else {
+	      $('#charNum').text(500 - len);
+	    }
+    };
 })
