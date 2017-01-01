799647375558dae9d25e1b0b4d988f5209b006a0
jequintero <eduardo.quintero52@gmail.com>
2017-01-01 10:49:18 -0700

    Error message

--- fbone/static/js/main.js
+++ fbone/static/js/main.js
@@ -10,9 +10,8 @@ $(document).ready(function() {
 	$('#reset_password_form').on('submit', function(e){
 		e.preventDefault();
 		var count = $("#password_input").val().length;
-
 		if(count >= 6){
-			e.submit():
+			e.submit();
 		}else{
 			$("#error_message").fadeIn();
 		}
