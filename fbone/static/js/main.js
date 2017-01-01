8dacd4c92f0f6064955d0f709f636f161a259586
jequintero <eduardo.quintero52@gmail.com>
2017-01-01 10:25:26 -0700

    Validations

--- fbone/static/js/main.js
+++ fbone/static/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
 	$('#reset_password_form').on('submit', function(e){
 		e.preventDefault();
-		var count = $("#reset_password_form").text().length;
+		var count = $("#password_input").text().length;
 		alert(count);
 		if(count >= 6){
 			alert("Simon");
