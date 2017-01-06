6ed0cbfb60d8db15fd7d1a9a3fd737f438928dfd
jequintero <eduardo.quintero52@gmail.com>
2017-01-05 17:37:19 -0700

    clean

--- fbone/coupon/models.py
+++ fbone/coupon/models.py
@@ -437,7 +437,8 @@ class ViewsLocationSchema(Schema):
                   'name',
                   'description',
                   'latitude',
-                  'longitude')
+                  'longitude',
+                  'available')
 
 class UsedCouponsByAge(Schema):
     class Meta:
@@ -477,7 +478,7 @@ coupons_taken_schema = CouponsTakenSchema(many=True)
 coupons_location_schema = CouponsLocation(many=True)
 coupons_views_schema = CouponsViews(many=True)
 
-views_location_schema = ViewsLocationSchema(many=True)
+views_location_schema = ViewsLocationSchema()
 
 taken_coupons_location_schema = TakenCouponsLocationSchema(many=True)
 
