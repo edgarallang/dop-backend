328767bf8c6e1e9f6decac5d2e86aa090fe50f82
jequintero <eduardo.quintero52@gmail.com>
2017-02-16 13:04:56 -0700

    branches folio

--- fbone/coupon/api.py
+++ fbone/coupon/api.py
@@ -580,7 +580,7 @@ def get_all_coupon_for_user_by_branch(branch_id):
     token_index = True
     payload = parse_token(request, token_index)
 
-    list_coupon = db.engine.execute('SELECT coupon_id, branches.branch_id, company_id, branches.name, coupon_folio, description, start_date, \
+    list_coupon = db.engine.execute('SELECT coupon_id, branches.branch_id, branches.folio,  company_id, branches.name, coupon_folio, description, start_date, \
                                             end_date, coupons.limit, min_spent, coupon_category_id, logo, latitude, longitude, banner, category_id, available, \
                                     (SELECT COUNT(*)  FROM coupons_likes \
                                         WHERE coupons.coupon_id = coupons_likes.coupon_id) AS total_likes, \
@@ -609,7 +609,7 @@ def get_all_coupon_for_user_by_branch_offset():
     branch_id = request.args.get('branch_id')
     payload = parse_token(request, token_index)
 
-    list_coupon = db.engine.execute('SELECT coupon_id, branches.branch_id, company_id, branches.name, coupon_folio, description, start_date, \
+    list_coupon = db.engine.execute('SELECT coupon_id, branches.branch_id, branches.folio, company_id, branches.name, coupon_folio, description, start_date, \
                                             end_date, coupons.limit, min_spent, coupon_category_id, logo, latitude, longitude, banner, category_id, available, \
                                     (SELECT COUNT(*)  FROM coupons_likes \
                                         WHERE coupons.coupon_id = coupons_likes.coupon_id) AS total_likes, \
