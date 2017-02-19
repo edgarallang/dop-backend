748faf0de690ef42c827743c830a8dc0c6293d16
edgarallanglez <edgarallanglez@gmail.com>
2017-02-19 11:47:57 -0700

    new global col in coupon schema

--- fbone/user/views.py
+++ fbone/user/views.py
@@ -684,7 +684,7 @@ def get_coupons_activity_by_user_likes():
             private = ''
 
         users = db.engine.execute('SELECT coupons.owner_id,coupons.coupon_id,branches_design.logo,coupons.name,clients_coupon.clients_coupon_id,clients_coupon.latitude,clients_coupon.longitude \
-                                    , users.names, users.surnames, users.user_id, users_image.main_image, branches.name AS branch_name, branches.company_id, clients_coupon.used_date, clients_coupon.private, \
+                                    , users.names, users.surnames, users.user_id, users_image.main_image, branches.branch_id, branches.name AS branch_name, branches.company_id, clients_coupon.used_date, clients_coupon.private, \
                                     (SELECT COUNT(*)  FROM clients_coupon_likes WHERE clients_coupon.clients_coupon_id = clients_coupon_likes.clients_coupon_id) AS total_likes, \
                                     (SELECT EXISTS (SELECT * FROM clients_coupon_likes WHERE clients_coupon_likes.user_id = %s AND clients_coupon_likes.clients_coupon_id = clients_coupon.clients_coupon_id)::bool) AS user_like \
                                     FROM clients_coupon \
@@ -716,8 +716,8 @@ def get_used_coupons_by_user_likes_offset():
             private = ''
 
         users = db.engine.execute('SELECT coupons.owner_id, coupons.coupon_id, branches_design.logo, coupons.name, \
-                                            clients_coupon.clients_coupon_id, clients_coupon.latitude,clients_coupon.longitude, \
-                                            users.names, users.surnames, users.user_id, users_image.main_image, branches.name AS branch_name, branches.company_id,clients_coupon.used_date, clients_coupon.private, \
+                                            clients_coupon.clients_coupon_id, clients_coupon.latitude,clients_coupon.longitude, branches.branch_id, \
+                                            users.names, users.surnames, users.user_id, users_image.main_image, branches.name AS branch_name, branches.company_id, clients_coupon.used_date, clients_coupon.private, \
                                     (SELECT COUNT(*)  FROM clients_coupon_likes WHERE clients_coupon.clients_coupon_id = clients_coupon_likes.clients_coupon_id) AS total_likes, \
                                     (SELECT EXISTS (SELECT * FROM clients_coupon_likes WHERE clients_coupon_likes.user_id = %d AND clients_coupon_likes.clients_coupon_id = clients_coupon.clients_coupon_id)::bool) AS user_like \
                                     FROM clients_coupon \
