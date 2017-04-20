6cac5f0831d06bbc645976423f2cc585d8e98057
jequintero <eduardo.quintero52@gmail.com>
2017-04-20 12:53:21 -0600

    Birthday fix

--- fbone/user/views.py
+++ fbone/user/views.py
@@ -340,7 +340,7 @@ def facebook_login():
 
     is_adult = False
 
-    if 'birth_date' in request.json and request.json['birthday'] != '':
+    if 'birth_date' in request.json and request.json['birth_date'] != '':
         birth_date = request.json['birth_date']
         is_adult = calculate_age(datetime.strptime(birth_date, "%m/%d/%Y"))
 
