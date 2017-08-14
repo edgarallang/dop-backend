b029bd712ecd6d9c8955aa28d665c03b707733f7
jequintero <eduardo.quintero52@gmail.com>
2017-08-14 13:15:08 -0600

    JWT

--- fbone/company/api.py
+++ fbone/company/api.py
@@ -29,8 +29,8 @@ def create_token(user):
     }
     payload = {
         'id': user.branches_user_id,
-        'iat': datetime.now()#,
-        #'exp': datetime.now() + timedelta(days=99)
+        'iat': datetime.now(),
+        'exp': datetime.now() + timedelta(days=99)
     }
 
     token = jwt.encode(payload, app.config['TOKEN_SECRET'], options=options)
