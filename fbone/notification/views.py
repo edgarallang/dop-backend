0d5772207fb9fab87e15201385806159ad1a2d49
Eduardo Quintero <eduardo.quintero52@gmail.com>
2017-10-09 00:04:54 -0600

    Join room

--- fbone/notification/views.py
+++ fbone/notification/views.py
@@ -366,7 +366,7 @@ def on_join_room(message):
 
 @socketio.on('waitingForRedeemWebAdmin')
 def on_waitin_for_redeem_web(branch):
-    branch_object = json.loads(branch)
+    #branch_object = json.loads(branch)
     #print branch_object.get('branch_id')
     #room = branch_object.get('branch_id')
     #join_room(room)
